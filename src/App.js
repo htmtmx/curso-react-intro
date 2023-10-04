e99d034c7811accba1f46a8377d1d2a28e628cb3
htmtmx <cesar.pineda@daksha.mx>
2023-10-04 16:43:13 -0600

    03 - Sending components props

--- src/App.js
+++ src/App.js
@@ -7,15 +7,34 @@ import './App.css';
 import { TodoItem } from './TodoItem';
 import React from 'react';
 
+const defaultTodos = [
+	{
+		text: "Cortar cebolla",
+		completed: false,
+	},
+	{
+		text: "Terminar curso de Reactjs",
+		completed: false,
+	},
+	{
+		text: "Hacer el super",
+		completed: false,
+	},
+	{
+		text: "Lavar el auto",
+		completed: false,
+	},
+];
+
 function App() {
     return (
     <React.Fragment>
         <TodoCounter completed={16} total={30} />
         <TodoSearch />
-        <TodoList>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            <TodoList>
+                {defaultTodos.map(todo => (
+                    <TodoItem key={todo.text} text={ todo.text} completed= {todo.completed} />
+                ))}
         </TodoList>
         <CreateTodoButton />
     </React.Fragment>
