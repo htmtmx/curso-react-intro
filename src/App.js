e943ed39ecca71bc2fddf661d153b31d4c57b9d0
htmtmx <cesar.pineda@daksha.mx>
2023-10-04 16:30:41 -0600

    02 - Using React.Fragment to render some elements

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import { TodoSearch } from './TodoSearch';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
 import { TodoItem } from './TodoItem';
+import React from 'react';
 
 function App() {
     return (
-        <div className="App">
-            <TodoCounter />
-            <TodoSearch />
-            <TodoList>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-            </TodoList>
-            <CreateTodoButton />
-        </div>
+    <React.Fragment>
+        <TodoCounter completed={16} total={30} />
+        <TodoSearch />
+        <TodoList>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </TodoList>
+        <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
